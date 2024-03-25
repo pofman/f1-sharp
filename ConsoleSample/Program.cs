99cdff72a0d5d8a3876334f2f526f65a01d5381b
Martin Mendez <g3xbetony@gmail.com>
2024-03-25 15:23:36 -0300

    Testing Lap data

--- ConsoleSample/Program.cs
+++ ConsoleSample/Program.cs
@@ -14,9 +14,33 @@ namespace ConsoleSample
             TelemetryClient client = new(20777);
 
             client.OnCarDamageDataReceive += Client_OnCarDamageDataReceive;
+            client.OnLapDataReceive += Client_OnLapDataReceive;
 
             Console.CursorVisible = false;
             Console.Read();
+        }
+
+        private static void Client_OnLapDataReceive(LapDataPacket packet)
+        {
+            int index = 0;
+            Console.Clear();
+            foreach (LapData data in packet.lapData)
+            {
+                var lastLapTime = TimeSpan.FromMilliseconds(data.lastLapTimeInMS);
+                Console.WriteLine($"INDEX: {index}");
+                Console.WriteLine($"Sectory 1: {data.sector1TimeInMS} ms");
+                Console.WriteLine($"Sectory 2: {data.sector2TimeInMS} ms");
+                Console.WriteLine($"Sectory 3: - ms");
+                Console.WriteLine($"Last Lap: {lastLapTime.TotalMinutes}:{lastLapTime.TotalSeconds}:{lastLapTime.TotalMilliseconds} ms");
+                Console.WriteLine("----");
+                index++;
+                if (index == 5)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"{packet.lapData[packet.header.playerCarIndex]}");
         }
 
         private static void Client_OnCarDamageDataReceive(CarDamagePacket packet)
